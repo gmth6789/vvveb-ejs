934b8295fd392929fbc491861aa2c9fb2c93ad51
gmth6789 <gmth.6789@gmail.com>
2024-06-24 14:44:40 +0700

    main

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const port = 3000;
 
 // LINE Bot configuration
 const config = {
-  channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
-  channelSecret: 'YOUR_CHANNEL_SECRET'
+  channelAccessToken: 'ZATSCX7jJfr0JNIBh08SOulayhT+ylUfODRDdB3nKUsx22zf4/Kfb7kkAVTHPfYkBqvjFRNULFo0b3e3BcHlg7fa06O2DaTfAhunVG4ToIxCgWTYzcS7XA8Jmi8b6btAoCNdSQkrlPalv4ScRoJd9wdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '6ac867326519450a35406ab44a6bef19'
 };
 
 const client = new Client(config);
